098ecc9084bd3de153a964d3d3f93b2471d71e15
SimBoo <thisiswoa@163.com>
2014-10-29 11:39:05 +0800

    branch 1基本文件

---
+++ app.js
@@ -0,0 +1,7 @@
+/* requires firstly*/
+var fs = require('fs-extra');
+var util = require('util');
+var cheerio = require('cheerio');
+var findit = require('findit');
+
+console.log("i am branch 1");
